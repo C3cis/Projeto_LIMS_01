53e5bee863a6adeff0c8b3cf86fa4293621083a0
Cecilia Russano <cecistudy23@gmail.com>
2025-11-18 12:06:17 +0000

    Fazendo atualizacao

--- Projeto 2 Semestre v04beta/Projeto 2 Semestre/LIMS/src/Pck_Model_LIMS/Model_Manutencao_05.java
+++ Projeto 2 Semestre v04beta/Projeto 2 Semestre/LIMS/src/Pck_Model_LIMS/Model_Manutencao_05.java
@@ -2,55 +2,98 @@ package Pck_Model_LIMS;
 
 import java.util.Date;
 
-
 public class Model_Manutencao_05 {
+
     private int a05_id_manutencao;
-    private int a05_id_produto;
-    private int a05_id_localizacao;
-    private int a05_id_usuario;
     private Date a05_data_manutencao;
     private String a05_tipo_manutencao;
     private String a05_descricao;
-    private String a05_menu_relatorio;
+    private String a05_status_resultado;
+    private String a05_relatorio;
+    private int a05_id_projeto;
+    private int a05_id_usuario;
+    private int a05_id_localizacao;
+    private int a05_id_produto;
 
-
-    public Model_Manutencao_05() {}
-
-
-    public Model_Manutencao_05(int a05_id_manutencao, int a05_id_produto, int a05_id_localizacao,
-                                int a05_id_usuario, Date a05_data_manutencao, String a05_tipo_manutencao,
-                                String a05_descricao, String a05_menu_relatorio) {
-        this.a05_id_manutencao = a05_id_manutencao;
-        this.a05_id_produto = a05_id_produto;
-        this.a05_id_localizacao = a05_id_localizacao;
-        this.a05_id_usuario = a05_id_usuario;
-        this.a05_data_manutencao = a05_data_manutencao;
-        this.a05_tipo_manutencao = a05_tipo_manutencao;
-        this.a05_descricao = a05_descricao;
-        this.a05_menu_relatorio = a05_menu_relatorio;
+    public int getA05_id_manutencao() {
+        return a05_id_manutencao;
     }
 
+    public void setA05_id_manutencao(int a05_id_manutencao) {
+        this.a05_id_manutencao = a05_id_manutencao;
+    }
 
-    // SET
-    public void setA05_id_manutencao(int a05_id_manutencao) { this.a05_id_manutencao = a05_id_manutencao; }
-    public void setA05_id_produto(int a05_id_produto) { this.a05_id_produto = a05_id_produto; }
-    public void setA05_id_localizacao(int a05_id_localizacao) { this.a05_id_localizacao = a05_id_localizacao; }
-    public void setA05_id_usuario(int a05_id_usuario) { this.a05_id_usuario = a05_id_usuario; }
-    public void setA05_data_manutencao(Date a05_data_manutencao) { this.a05_data_manutencao = a05_data_manutencao; }
-    public void setA05_tipo_manutencao(String a05_tipo_manutencao) { this.a05_tipo_manutencao = a05_tipo_manutencao; }
-    public void setA05_descricao(String a05_descricao) { this.a05_descricao = a05_descricao; }
-    public void setA05_menu_relatorio(String a05_menu_relatorio) { this.a05_menu_relatorio = a05_menu_relatorio; }
+    public Date getA05_data_manutencao() {
+        return a05_data_manutencao;
+    }
 
+    public void setA05_data_manutencao(Date a05_data_manutencao) {
+        this.a05_data_manutencao = a05_data_manutencao;
+    }
 
-    // GET
-    public int getA05_id_manutencao() { return a05_id_manutencao; }
-    public int getA05_id_produto() { return a05_id_produto; }
-    public int getA05_id_localizacao() { return a05_id_localizacao; }
-    public int getA05_id_usuario() { return a05_id_usuario; }
-    public Date getA05_data_manutencao() { return a05_data_manutencao; }
-    public String getA05_tipo_manutencao() { return a05_tipo_manutencao; }
-    public String getA05_descricao() { return a05_descricao; }
-    public String getA05_menu_relatorio() { return a05_menu_relatorio; }
+    public String getA05_tipo_manutencao() {
+        return a05_tipo_manutencao;
+    }
+
+    public void setA05_tipo_manutencao(String a05_tipo_manutencao) {
+        this.a05_tipo_manutencao = a05_tipo_manutencao;
+    }
+
+    public String getA05_descricao() {
+        return a05_descricao;
+    }
+
+    public void setA05_descricao(String a05_descricao) {
+        this.a05_descricao = a05_descricao;
+    }
+
+    public String getA05_status_resultado() {
+        return a05_status_resultado;
+    }
+
+    public void setA05_status_resultado(String a05_status_resultado) {
+        this.a05_status_resultado = a05_status_resultado;
+    }
+
+    public String getA05_relatorio() {
+        return a05_relatorio;
+    }
+
+    public void setA05_relatorio(String a05_relatorio) {
+        this.a05_relatorio = a05_relatorio;
+    }
+
+    public int getA05_id_projeto() {
+        return a05_id_projeto;
+    }
+
+    public void setA05_id_projeto(int a05_id_projeto) {
+        this.a05_id_projeto = a05_id_projeto;
+    }
+
+    public int getA05_id_usuario() {
+        return a05_id_usuario;
+    }
+
+    public void setA05_id_usuario(int a05_id_usuario) {
+        this.a05_id_usuario = a05_id_usuario;
+    }
+
+    public int getA05_id_localizacao() {
+        return a05_id_localizacao;
+    }
+
+    public void setA05_id_localizacao(int a05_id_localizacao) {
+        this.a05_id_localizacao = a05_id_localizacao;
+    }
+
+    public int getA05_id_produto() {
+        return a05_id_produto;
+    }
+
+    public void setA05_id_produto(int a05_id_produto) {
+        this.a05_id_produto = a05_id_produto;
+    }
 }
 
 
--- Projeto 2 Semestre v04beta/Projeto 2 Semestre/LIMS/src/Pck_Persistencia_LIMS/Persistencia_Manutencao_05.java
+++ Projeto 2 Semestre v04beta/Projeto 2 Semestre/LIMS/src/Pck_Persistencia_LIMS/Persistencia_Manutencao_05.java
@@ -1,9 +1,121 @@
 package Pck_Persistencia_LIMS;
 
-import java.util.Date;
+import model.Model_Manutencao_05;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class Persistencia_Manutencao_05 {
+    
+    private Connection connection;
+
+    public Persistencia_Manutencao_05(Connection connection) {
+        this.connection = connection;
+    }
+
+    // INSERT
+    public void inserir(Model_Manutencao_05 m) throws Exception {
+        CallableStatement stmt = connection.prepareCall("{CALL SP_INSERIR_MANUTENCAO_05(?,?,?,?,?,?,?,?,?)}");
+
+        stmt.setDate(1, new java.sql.Date(m.getA05_data_manutencao().getTime()));
+        stmt.setString(2, m.getA05_tipo_manutencao());
+        stmt.setString(3, m.getA05_descricao());
+        stmt.setString(4, m.getA05_status_resultado());
+        stmt.setString(5, m.getA05_relatorio());
+        stmt.setInt(6, m.getA05_id_projeto());
+        stmt.setInt(7, m.getA05_id_usuario());
+        stmt.setInt(8, m.getA05_id_localizacao());
+        stmt.setInt(9, m.getA05_id_produto());
+
+        stmt.execute();
+        stmt.close();
+    }
+
+    // UPDATE
+    public void atualizar(Model_Manutencao_05 m) throws Exception {
+        CallableStatement stmt = connection.prepareCall("{CALL SP_ATUALIZAR_MANUTENCAO_05(?,?,?,?,?,?,?,?,?,?)}");
+
+        stmt.setInt(1, m.getA05_id_manutencao());
+        stmt.setDate(2, new java.sql.Date(m.getA05_data_manutencao().getTime()));
+        stmt.setString(3, m.getA05_tipo_manutencao());
+        stmt.setString(4, m.getA05_descricao());
+        stmt.setString(5, m.getA05_status_resultado());
+        stmt.setString(6, m.getA05_relatorio());
+        stmt.setInt(7, m.getA05_id_projeto());
+        stmt.setInt(8, m.getA05_id_usuario());
+        stmt.setInt(9, m.getA05_id_localizacao());
+        stmt.setInt(10, m.getA05_id_produto());
+
+        stmt.execute();
+        stmt.close();
+    }
+
+    // DELETE
+    public void excluir(int id) throws Exception {
+        CallableStatement stmt = connection.prepareCall("{CALL SP_EXCLUIR_MANUTENCAO_05(?)}");
+        stmt.setInt(1, id);
+        stmt.execute();
+        stmt.close();
+    }
+
+    // SEARCH
+    public Model_Manutencao_05 buscar(int id) throws Exception {
+        CallableStatement stmt = connection.prepareCall("{CALL SP_BUSCAR_MANUTENCAO_05(?)}");
+        stmt.setInt(1, id);
+
+        ResultSet rs = stmt.executeQuery();
+
+        Model_Manutencao_05 m = null;
+
+        if (rs.next()) {
+            m = new Model_Manutencao_05();
+            m.setA05_id_manutencao(rs.getInt("A05_ID_MANUTENCAO"));
+            m.setA05_data_manutencao(rs.getDate("A05_DATA_MANUTENCAO"));
+            m.setA05_tipo_manutencao(rs.getString("A05_TIPO_MANUTENCAO"));
+            m.setA05_descricao(rs.getString("A05_DESCRICAO"));
+            m.setA05_status_resultado(rs.getString("A05_STATUS_RESULTADO"));
+            m.setA05_relatorio(rs.getString("A05_RELATORIO"));
+            m.setA05_id_projeto(rs.getInt("A05_ID_PROJETO"));
+            m.setA05_id_usuario(rs.getInt("A05_ID_USUARIO"));
+            m.setA05_id_localizacao(rs.getInt("A05_ID_LOCALIZACAO"));
+            m.setA05_id_produto(rs.getInt("A05_ID_PRODUTO"));
+        }
+
+        rs.close();
+        stmt.close();
+        return m;
+    }
+
+    // LIST
+    public List<Model_Manutencao_05> listar() throws Exception {
+        List<Model_Manutencao_05> lista = new ArrayList<>();
+
+        CallableStatement stmt = connection.prepareCall("{CALL SP_LISTAR_MANUTENCAO_05()}");
+        ResultSet rs = stmt.executeQuery();
+
+        while (rs.next()) {
+            Model_Manutencao_05 m = new Model_Manutencao_05();
+
+            m.setA05_id_manutencao(rs.getInt("A05_ID_MANUTENCAO"));
+            m.setA05_data_manutencao(rs.getDate("A05_DATA_MANUTENCAO"));
+            m.setA05_tipo_manutencao(rs.getString("A05_TIPO_MANUTENCAO"));
+            m.setA05_descricao(rs.getString("A05_DESCRICAO"));
+            m.setA05_status_resultado(rs.getString("A05_STATUS_RESULTADO"));
+            m.setA05_relatorio(rs.getString("A05_RELATORIO"));
+            m.setA05_id_projeto(rs.getInt("A05_ID_PROJETO"));
+            m.setA05_id_usuario(rs.getInt("A05_ID_USUARIO"));
+            m.setA05_id_localizacao(rs.getInt("A05_ID_LOCALIZACAO"));
+            m.setA05_id_produto(rs.getInt("A05_ID_PRODUTO"));
+
+            lista.add(m);
+        }
+
+        rs.close();
+        stmt.close();
+        return lista;
+    }
 }
+
 
 
